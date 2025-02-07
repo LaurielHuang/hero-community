1aece6eafa81a728cadeaa8758a6a7404fbe4901
Lauriel Huang <lauriel.huang@heroeew.com>
2025-02-07 17:44:44 +0800

    v

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,10 +3,10 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "59c470b558ea8c4756ca8c437864fbf6",
+const RESOURCES = {"flutter_bootstrap.js": "225d6ea60fd6fb05085abcf91893d3af",
 "version.json": "404b1d0c23967606f0ff6aa720ee261e",
-"index.html": "a8d9cbe7eb43bacd86e8df41ff294ef9",
-"/": "a8d9cbe7eb43bacd86e8df41ff294ef9",
+"index.html": "3f0890eb940161efb9b7a6dd5dc3cb07",
+"/": "3f0890eb940161efb9b7a6dd5dc3cb07",
 "main.dart.js": "e789f34c73acd6fb20dc94cb72147395",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
 "favicon.png": "e5393f83f41c3141e855d1898d4578b0",
